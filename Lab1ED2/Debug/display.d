c65c021507be875b6ce29c61fd6840820e91c362
josepappa <arg23149@uvg.edu.gt>
2026-01-22 20:05:06 -0600

    Add files via upload

---
+++ Lab1ED2/Debug/display.d
@@ -0,0 +1,9 @@
+display.d display.o: .././display.c .././display.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
+
+.././display.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
